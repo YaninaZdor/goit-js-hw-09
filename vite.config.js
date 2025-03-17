6a2da6378448b540ff6ab43a301268a66e249465
Yanina Zdor <152975349+YaninaZdor@users.noreply.github.com>
2025-03-17 09:59:50 +0200

    all files

--- vite.config.js
+++ vite.config.js
@@ -12,28 +12,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
-      },
       outDir: '../dist',
       emptyOutDir: true,
     },
